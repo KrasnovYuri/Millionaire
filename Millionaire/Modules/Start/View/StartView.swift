6aa2dbfc6593cd8aeefc903414588120fda26c79
koko6a <rus.baranovskiy@gmail.com>
2024-02-25 20:02:44 +0300

    complete

--- Millionaire/Modules/Start/View/StartView.swift
+++ Millionaire/Modules/Start/View/StartView.swift
@@ -2,11 +2,13 @@ import SwiftUI
 import Questions
 
 struct StartView: View {
+    let quest: QuestionStorageProtocol = QuestionStorage()
     var body: some View {
+        Button("Test") {
+            quest.fetchAllQuestions { questions in
+                print(questions)
+            }
+        }
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-}
-
-#Preview {
-    StartView()
 }

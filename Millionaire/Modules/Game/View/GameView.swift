90461df2bbb35321f2845288099311a730808da8
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-02-26 13:16:13 +0530

    add

--- Millionaire/Modules/Game/View/GameView.swift
+++ Millionaire/Modules/Game/View/GameView.swift
@@ -5,3 +5,9 @@ struct GameView: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}

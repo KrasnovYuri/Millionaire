2a684a2324fd0191915dde9bb0898cbe3f483179
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-02-26 19:20:05 +0530

    add AnswerButtonView, HintButtonsView, GameView design.

--- Millionaire/Modules/Game/View/GameView.swift
+++ Millionaire/Modules/Game/View/GameView.swift
@@ -2,7 +2,62 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            GradientBackgroundView()
+            VStack {
+                Image("logoLarge")
+                    .resizable()
+                    .frame(width: 153, height: 158)
+                    .scaledToFit()
+                    .aspectRatio(contentMode: .fit)
+                    .colorMultiply(Color.gray.opacity(0.8))
+                
+                HintButtonsView()
+                
+                HStack {
+                    Text("Вопрос 1")
+                        .foregroundColor(.white)
+                        .font(.system(size: 28, weight: .semibold))
+                    Spacer()
+                    HStack {
+                        Image("cash")
+                        Text("500₽")
+                            .foregroundColor(.white)
+                            .font(.system(size: 22, weight: .semibold))
+                    }
+                }
+                .padding()
+                .padding(.top, 10)
+                
+                Text("О чём писал Грибоедов, отмечая, что он «нам сладок и приятен»")
+                    .foregroundColor(.white)
+                    .font(.system(size: 16, weight: .regular))
+                    .frame(width: 328, height: 122)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 15)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .foregroundStyle(LinearGradient(colors: [.indigo, .black], startPoint: .topLeading, endPoint: .bottomTrailing))
+                    )
+                    .padding(.top, 10)
+                
+                VStack(spacing: 18) {
+                    AnswerButtonView(label: "A.", text: "Дым Отечества") {
+                        // action
+                    }
+                    AnswerButtonView(label: "B.", text: "Дух купечества") {
+                        // action
+                    }
+                    AnswerButtonView(label: "C.", text: "Дар пророчества") {
+                        // action
+                    }
+                    AnswerButtonView(label: "D.", text: "Пыл девичества") {
+                        // action
+                    }
+                }
+                .padding(.top, 30)
+            }
+        }
     }
 }
 
@@ -11,3 +66,4 @@ struct GameView_Previews: PreviewProvider {
         GameView()
     }
 }
+

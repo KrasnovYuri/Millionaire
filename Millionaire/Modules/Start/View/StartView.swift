001c0b0d81696bf5bb3595557c544b735907e5e9
KrasnovYuri <krasnov1yuri@gmail.com>
2024-02-26 13:26:29 +0300

    fix

--- Millionaire/Modules/Start/View/StartView.swift
+++ Millionaire/Modules/Start/View/StartView.swift
@@ -5,9 +5,47 @@ struct StartView: View {
         ZStack {
             GradientBackgroundView()
             
-            HStack {
-                Text("Hello")
-                    .foregroundStyle(.white)
+            VStack {
+                Spacer()
+                Spacer()
+                
+                Image(.logoLarge)
+                Text("Добро пожаловать в игру")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Text("КТО ХОЧЕТ СТАТЬ \n МИЛЛИОНЕРОМ")
+                    .font(.title)
+                    .foregroundStyle(LinearGradient(colors: [.purple, .blue], startPoint: .leading, endPoint: .trailing))
+                
+                Spacer()
+                VStack {
+                    Button {
+                        print("Правила игры")
+                    } label: {
+                        Text("Правила игры")
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 200)
+                    .background(.purple)
+                    .cornerRadius(10)
+                    
+                    
+                    Button {
+                        print("Играть")
+                    } label: {
+                        Text("Играть")
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 200)
+                    .background(.purple)
+                    .cornerRadius(10)
+                }
+                Spacer()
             }
         }
     }

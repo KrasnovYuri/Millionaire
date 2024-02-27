9aa083083e77c7bd69bfa2afdcafa1235a4fc27a
Пахаев Хусейн <pakhaev01@gmail.com>
2024-02-27 20:44:43 +0300

    Create SoundManager class & SoundType enum

--- Millionaire/Modules/Game/Helpers/SoundType.swift
+++ Millionaire/Modules/Game/Helpers/SoundType.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SoundManager
+
+enum SoundType: String {
+    case correct = "correctAnswer"
+    case incorrect = "incorrectAnswer"
+    case pauseUntilResult
+    case win = "playerWin"
+    case timer = "timeOn"
+    
+    func play() {
+        do {
+            try SoundManager.shared.play(filename: self.rawValue)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func pause() {
+        SoundManager.shared.pause()
+    }
+    
+    func stop() {
+        SoundManager.shared.stop()
+    }
+}
--- Millionaire/Packages/SoundManager/Sources/SoundManager/SoundManager.swift
+++ Millionaire/Packages/SoundManager/Sources/SoundManager/SoundManager.swift
@@ -1,2 +1,48 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import AVFoundation
+
+public final class SoundManager {
+    public static let shared = SoundManager()
+    private var player: AVAudioPlayer?
+    
+    private init() {}
+    
+    private enum SoundError: Error {
+        case notFound(name: String)
+        case dontPlaying(name: String)
+        
+        var localize: String {
+            switch self {
+            case .notFound(let name):
+                return "Аудиофайл \(name) не найден"
+            case .dontPlaying(let name):
+                return "Ошибка воспроизведения аудио: \(name)"
+            }
+        }
+    }
+    
+    public func play(filename: String) throws {
+        guard let data = NSDataAsset(name: filename, bundle: .module)?.data else {
+            throw SoundError.notFound(name: filename)
+        }
+        
+        do {
+            player = try AVAudioPlayer(data: data)
+            player?.play()
+        } catch {
+            throw SoundError.dontPlaying(name: filename)
+        }
+    }
+    
+    public func pause() {
+        player?.pause()
+    }
+    
+    public func stop() {
+        player?.stop()
+        player?.currentTime = 0
+    }
+}
